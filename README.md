ede2e23bc1e984bd09381e9c805c9c931a39b284
quadrifoglio <quadrifoglio.clement@protonmail.com>
2017-11-03 18:56:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ebml
+
+Simple implementation of the EBML format for the Rust Programming Language.
+
+# license
+
+wtfpl
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+/// This library is a basic implementation of EBML (Extensible Binary Markup Language),
+/// a binary format for storing hierarchical, typed in data in a compact, yet easily
+/// parsed format. It is used in the MKV Video container format.
