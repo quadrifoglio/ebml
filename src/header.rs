4ca26c0f41fa3bc69105136d42ad10345115d9c5
quadrifoglio <quadrifoglio.clement@protonmail.com>
2017-11-24 21:21:41 +0100

    added EBML header element IDs as constants

--- src/header.rs
+++ src/header.rs
@@ -2,13 +2,22 @@
 
 use element::types::*;
 
-ebml_container_element!(Root => 0x1a45dfa3);
+pub const EBML: UnsignedInt = 0x1a45dfa3;
+pub const VERSION: UnsignedInt = 0x4286;
+pub const READ_VERSION: UnsignedInt = 0x42f7;
+pub const MAX_ID_LENGTH: UnsignedInt = 0x42f2;
+pub const MAX_SIZE_LENGTH: UnsignedInt = 0x42f3;
+pub const DOC_TYPE: UnsignedInt = 0x4282;
+pub const DOC_TYPE_VERSION: UnsignedInt = 0x4287;
+pub const DOC_TYPE_READ_VERSION: UnsignedInt = 0x4285;
 
-ebml_default_element!(Version => 0x4286, UnsignedInt, 1);
-ebml_default_element!(ReadVersion => 0x42f7, UnsignedInt, 1);
-ebml_default_element!(MaxIdLength => 0x42f2, UnsignedInt, 4);
-ebml_default_element!(MaxSizeLength => 0x42f3, UnsignedInt, 8);
+ebml_container_element!(Root => EBML);
 
-ebml_mandatory_element!(DocType => 0x4282, Utf8);
-ebml_mandatory_element!(DocTypeVersion => 0x4287, UnsignedInt);
-ebml_mandatory_element!(DocTypeReadVersion => 0x4285, UnsignedInt);
+ebml_default_element!(Version => VERSION, UnsignedInt, 1);
+ebml_default_element!(ReadVersion => READ_VERSION, UnsignedInt, 1);
+ebml_default_element!(MaxIdLength => MAX_ID_LENGTH, UnsignedInt, 4);
+ebml_default_element!(MaxSizeLength => MAX_SIZE_LENGTH, UnsignedInt, 8);
+
+ebml_mandatory_element!(DocType => DOC_TYPE, Utf8);
+ebml_mandatory_element!(DocTypeVersion => DOC_TYPE_VERSION, UnsignedInt);
+ebml_mandatory_element!(DocTypeReadVersion => DOC_TYPE_READ_VERSION, UnsignedInt);
